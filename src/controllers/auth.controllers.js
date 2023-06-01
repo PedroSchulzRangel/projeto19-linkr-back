f53b65120e6656e88a134ff93ebb161f4a7ebaae
mickalala <mickalarissa01@gmail.com>
2023-06-01 12:00:59 -0300

    feature login, addinng functions to routes

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,8 +1,11 @@
+import { db } from "../database/database.connection.js"
+
 export async function sigIn(req, res) {
 
     const { email, password } = req.body
     try {
-
+        await db.query(`INSERT INTO sessions (email, password) VALUES ($1,$2);`,[email, password])
+        res.status(200).send("deubom")
     } catch (err) {
         res.status(500).send(err.message)
     }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,10 @@
 import { Router } from "express"
+import validateSigIn from "../middlewares/validateSigIn.js"
+import { sigInSchema } from "../schemas/sigIn.schema.js"
+import { sigIn } from "../controllers/auth.controllers.js"
 
-const authRouter= Router()
+const authRouter = Router()
 
-authRouter.post("/")
+authRouter.post("/", validateSigIn(sigInSchema), sigIn)
 
 export default authRouter
