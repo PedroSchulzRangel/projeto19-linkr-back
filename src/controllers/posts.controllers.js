a5a75ad5229fd3c4b28d89a0f9078b52b627cab1
oliveiratullio <105726160+oliveiratullio@users.noreply.github.com>
2023-06-08 19:48:47 -0300

    feat: delete and edit posts (#14)
    * feat: delete and edit posts
    
    * atualização de variáveis

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -1,13 +1,33 @@
-import { createPostDB } from "../repository/posts.repositories.js";
+import { createPostDB, deletePostDB, editPostDB } from "../repository/posts.repositories.js";
 
 
 export async function createPost(req, res){
     const {description, url} = req.body;
     const user = res.locals.user;
     try {
-        const post = await createPostDB(user.id, url, description);
+        await createPostDB(user.id, url, description);
         return res.sendStatus(201);        
-    } catch (error) {
-        return res.status(500).send(error.message);
+    } catch (err) {
+        return res.status(500).send(err.message);
     }
+}
+export async function deletePost(req, res){
+    const user = res.locals.user;
+    try {        
+         await deletePostDB(req.params,user.id);
+        res.sendStatus(200);
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+}
+
+export async function editPost(req,res){
+    const { description } = req.body;
+    const { id } = req.params;
+    try {
+        await editPostDB(description, id);
+        res.sendStatus(200)
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }    
 }
--- src/repository/posts.repositories.js
+++ src/repository/posts.repositories.js
@@ -4,3 +4,13 @@ export async function createPostDB(userId, url, description){
     const results = db.query(`INSERT INTO "posts" ("userId", "url", "description") VALUES ($1, $2, $3) RETURNING id;`, [userId, url, description]);
     return results;
 }
+
+export async function deletePostDB(params, userId) {
+    const { postId } = params;
+    const results = await db.query(`DELETE FROM posts WHERE id=$1 AND "userId"=$2`, [postId, userId]);
+    return results
+}
+export function editPostDB(id, description) {
+	const results = db.query(`UPDATE postsSET description = $1 WHERE id = $2 RETURNING *`, [description, id]);
+    return results
+}
