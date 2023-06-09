2a229e0937f82a43c46ba4aaddff4e663fdf560e
JoaoAlgeri <joaoalgeri@gmail.com>
2023-06-08 23:18:10 -0300

    feat:Get users by username

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express"
-import { getUserById } from "../controllers/users.controllers.js"
+import { getUserById,  getUsersByName } from "../controllers/users.controllers.js"
 
 const usersRouter = Router()
 
 usersRouter.get("/user/:id", getUserById)
+usersRouter.get("/users", getUsersByName)
 
 export default usersRouter
