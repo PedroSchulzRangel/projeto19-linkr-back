fcc4308c8d1e2abbe0c81a1877f17970c37c5679
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-06-08 21:09:28 -0300

    fix: column name linkUrl added to query in createPostDB

--- src/repository/posts.repositories.js
+++ src/repository/posts.repositories.js
@@ -1,7 +1,7 @@
 import { db } from "../database/database.connection.js";
 
 export async function createPostDB(userId, url, description){
-    const results = db.query(`INSERT INTO "posts" ("userId", "url", "description") VALUES ($1, $2, $3) RETURNING id;`, [userId, url, description]);
+    const results = db.query(`INSERT INTO "posts" ("userId", "linkUrl", "description") VALUES ($1, $2, $3) RETURNING id;`, [userId, url, description]);
     return results;
 }
 
